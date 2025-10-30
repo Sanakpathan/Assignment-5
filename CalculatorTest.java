c34f91d3db1a9eda404d01b5b71cab03d234132d
SanaPathan <pathss@farmingdale.edu>
2025-10-30 19:21:37 -0400

    Create CalculatorTest.java

---
+++ CalculatorTest.java
@@ -0,0 +1,33 @@
+// CalculatorTest.java
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    Calculator calc = new Calculator();
+
+    @Test
+    void add_returnsSumOfTwoNumbers() {
+        assertEquals(7, calc.add(3, 4));
+    }
+
+    @Test
+    void subtract_returnsDifferenceOfTwoNumbers() {
+        assertEquals(1, calc.subtract(5, 4));
+    }
+
+    @Test
+    void multiply_returnsProductOfTwoNumbers() {
+        assertEquals(20, calc.multiply(5, 4));
+    }
+
+    @Test
+    void divide_returnsQuotientOfTwoNumbers() {
+        assertEquals(2.5, calc.divide(5, 2), 0.0001);
+    }
+
+    @Test
+    void divide_byZero_throwsException() {
+        assertThrows(IllegalArgumentException.class, () -> calc.divide(5, 0));
+    }
+}
